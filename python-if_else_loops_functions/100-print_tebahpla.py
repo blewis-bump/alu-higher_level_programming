b0c27316fbd6a9a2f734f9da967507cdefbbe30f
blewis-bump <b.lewis@alustudent.com>
2026-02-26 02:33:08 -0800

    advanced task

--- python-if_else_loops_functions/100-print_tebahpla.py
+++ python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 for i in range(25, -1, -1):
-    c = chr(ord('a') + i) if i % 2 == 0 else chr(ord('A') + i)
+    if (25 - i) % 2 == 0:
+        c = chr(ord('a') + i)
+    else:
+        c = chr(ord('A') + i)
     print("{}".format(c), end="")
