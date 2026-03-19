824b906864a00b1973d54e4ab8a77a953f22d6b3
blewis-bump <b.lewis@alustudent.com>
2026-03-19 05:32:17 -0700

    python class

---
+++ python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Module that defines a Square class with a private size attribute."""
+
+
+class Square:
+    """Class that defines a square."""
+
+    def __init__(self, size):
+        """Initialize a square with a private size attribute."""
+        self.__size = size
