afc09244c9ac568565e45f0f2bb6ce331d4c01fa
blewis-bump <blewis-bump@students.alu.edu>
2026-03-26 14:21:06 -0700

    Fix task 1: use SET PASSWORD syntax to match checker

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,5 +1,5 @@
 -- Task 1: Create MySQL server user user_0d_1 with all privileges
-DROP USER IF EXISTS 'user_0d_1'@'localhost';
-CREATE USER 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost';
+SET PASSWORD FOR 'user_0d_1'@'localhost' = 'user_0d_1_pwd';
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
 FLUSH PRIVILEGES;
