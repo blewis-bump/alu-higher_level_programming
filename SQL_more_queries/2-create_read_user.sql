179b8bfde81b52877e71a086bab28108299b9ead
blewis-bump <blewis-bump@students.alu.edu>
2026-03-29 09:33:26 -0700

    fix: 2files

--- SQL_more_queries/2-create_read_user.sql
+++ SQL_more_queries/2-create_read_user.sql
@@ -1,4 +1,4 @@
--- Task 2: Create database hbtn_0d_2 and user user_0d_2 with SELECT privilege
+-- Creates database hbtn_0d_2 and user user_0d_2 with SELECT privilege
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
 GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
