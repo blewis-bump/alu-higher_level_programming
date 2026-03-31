70611089e2de36c014f220e4d2c5171f568c0965
blewis-bump <blewis-bump@students.alu.edu>
2026-03-31 07:13:57 -0700

    Fix 0 and 4: add SSL context for urllib, keep hardcoded URL

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,11 +1,15 @@
 #!/usr/bin/python3
 """Fetches https://alx-intranet.hbtn.io/status using urllib."""
 import urllib.request
+import ssl
 
 
 if __name__ == "__main__":
     url = "https://alx-intranet.hbtn.io/status"
-    with urllib.request.urlopen(url) as response:
+    ctx = ssl.create_default_context()
+    ctx.check_hostname = False
+    ctx.verify_mode = ssl.CERT_NONE
+    with urllib.request.urlopen(url, context=ctx) as response:
         body = response.read()
     print("Body response:")
     print("	- type: {}".format(type(body)))
