80911c3d9c43382df0f2e847a61360f01fe19e87
blewis-bump <blewis-bump@students.alu.edu>
2026-03-31 07:20:38 -0700

    Fix 0-hbtn_status.py: use correct intranet.hbtn.io URL

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-"""Fetches https://alx-intranet.hbtn.io/status using urllib."""
+"""Fetches https://intranet.hbtn.io/status using urllib."""
 import urllib.request
 
 
 if __name__ == "__main__":
-    url = "https://alx-intranet.hbtn.io/status"
+    url = "https://intranet.hbtn.io/status"
     with urllib.request.urlopen(url) as response:
         body = response.read()
     print("Body response:")
