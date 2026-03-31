b997681962aef8ce48c719a09c55eec654296ebf
blewis-bump <blewis-bump@students.alu.edu>
2026-03-31 07:18:38 -0700

    Fix 0-hbtn_status.py: remove ssl import, only use urllib

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,15 +1,11 @@
 #!/usr/bin/python3
 """Fetches https://alx-intranet.hbtn.io/status using urllib."""
 import urllib.request
-import ssl
 
 
 if __name__ == "__main__":
     url = "https://alx-intranet.hbtn.io/status"
-    ctx = ssl.create_default_context()
-    ctx.check_hostname = False
-    ctx.verify_mode = ssl.CERT_NONE
-    with urllib.request.urlopen(url, context=ctx) as response:
+    with urllib.request.urlopen(url) as response:
         body = response.read()
     print("Body response:")
     print("	- type: {}".format(type(body)))
