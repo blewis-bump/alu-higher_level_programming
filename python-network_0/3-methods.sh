9a5bd9cea6bf370d81eb0858c4cae3d8f455058b
blewis-bump <blewis-bump@students.alu.edu>
2026-03-31 06:54:26 -0700

    Fix 1-body.sh: add -L flag; fix 3-methods.sh: display Allow header value

--- python-network_0/3-methods.sh
+++ python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Takes in a URL and displays all HTTP methods the server will accept
-curl -s -X OPTIONS "$1" | xargs
+curl -s -X OPTIONS "$1" -I | grep "Allow:" | tr -d "" | cut -d " " -f2-
