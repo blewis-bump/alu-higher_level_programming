5c0a8c33f19199ce30717b90d8935b86ea876bd6
blewis-bump <blewis-bump@students.alu.edu>
2026-04-03 02:57:19 -0700

    Fix case handling in 1-filter_states and 2-my_filter_states

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Script that lists all states starting with N from hbtn_0e_0_usa."""
+"""Script that lists all states with name starting with N from hbtn_0e_0_usa."""
 import MySQLdb
 import sys
 
@@ -9,7 +9,9 @@ if __name__ == "__main__":
         user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3]
     )
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+    cur.execute(
+        "SELECT * FROM states WHERE UPPER(name) LIKE 'N%' ORDER BY id ASC"
+    )
     for row in cur.fetchall():
         print(row)
     cur.close()
--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -9,8 +9,10 @@ if __name__ == "__main__":
         user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3]
     )
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name = '{}' ORDER BY id ASC"
-                .format(sys.argv[4]))
+    cur.execute(
+        "SELECT * FROM states WHERE BINARY name = '{}' ORDER BY id ASC"
+        .format(sys.argv[4])
+    )
     for row in cur.fetchall():
         print(row)
     cur.close()
