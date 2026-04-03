68b44fce79a8bb00907d0f8ba0fc5079e01c4026
blewis-bump <blewis-bump@students.alu.edu>
2026-04-03 02:52:04 -0700

    Add python-object_relational_mapping project

---
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Script that lists states matching a name argument (not injection safe)."""
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(
+        host="localhost", port=3306,
+        user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3]
+    )
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE name = '{}' ORDER BY id ASC"
+                .format(sys.argv[4]))
+    for row in cur.fetchall():
+        print(row)
+    cur.close()
+    db.close()
