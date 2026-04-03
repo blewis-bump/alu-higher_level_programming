4a2c939cee89a418c71b30806223ade1b1e2ce09
blewis-bump <blewis-bump@students.alu.edu>
2026-04-03 03:00:35 -0700

    Use case-insensitive LIKE N% in 1-filter_states

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -10,7 +10,7 @@ if __name__ == "__main__":
     )
     cur = db.cursor()
     cur.execute(
-        "SELECT * FROM states WHERE UPPER(name) LIKE 'N%' ORDER BY id ASC"
+        "SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC"
     )
     for row in cur.fetchall():
         print(row)
